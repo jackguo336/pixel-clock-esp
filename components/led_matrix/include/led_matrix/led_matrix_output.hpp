d76b81d99aa79bb16c28642aae74dff18a73bf4a
Jack Guo <thetitanjack@gmail.com>
2026-09-25 20:34:02 -0400

    change default max brightness

--- components/led_matrix/include/led_matrix/led_matrix_output.hpp
+++ components/led_matrix/include/led_matrix/led_matrix_output.hpp
@@ -20,7 +20,7 @@ struct LedMatrixOutputConfig {
     // WARNING: 256 WS2812Bs at full brightness can draw tens of amps. Consider power
     // supply current limits before increasing this value. Maximum value of a color
     // channel, from 0 to 255.
-    uint8_t max_brightness{5};
+    uint8_t max_brightness{50};
 };
 
 enum class LedMatrixOutputStatus : uint8_t {
